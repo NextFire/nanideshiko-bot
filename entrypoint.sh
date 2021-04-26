8b99f7ed28c5cedde274d40715c6bc29ef35da08
NextFire <git@yuru.moe>
2021-04-26 18:11:07 +0200

    simplify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-mkdir config
-touch config/__init__.py
-
-mkdir config/res
-touch config/res/__init__.py
-
-mkdir config/saves
-touch config/saves/__init__.py
+mkdir -p config/res config/saves
+touch config/__init__.py config/res/__init__.py config/saves/__init__.py
 
 python nanideshiko.py
