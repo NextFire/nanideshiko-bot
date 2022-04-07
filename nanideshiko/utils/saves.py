a4a5c6b7444b39dfed938b34fe0ae820f8e9a0a7
NextFire <git@yuru.moe>
2022-04-07 19:07:22 +0200

    saves: logging

--- nanideshiko/utils/saves.py
+++ nanideshiko/utils/saves.py
@@ -1,16 +1,24 @@
 import json
-from contextlib import suppress
+import logging
 from importlib import resources
 
 from ..config import saves
 
+logger = logging.getLogger(__name__)
+
 
 def sload(key: str):
-    with suppress(FileNotFoundError):
-        return json.loads(resources.read_binary(saves, f"{key}.json"))
+    data = {}
+    try:
+        data = json.loads(resources.read_binary(saves, f"{key}.json"))
+        logger.info(f'Loading {key}')
+    except FileNotFoundError:
+        logger.info(f'{key} not found')
+    return data
 
 
 def sdump(key: str, data):
     with resources.path(saves, f"{key}.json") as p:
         with open(p, 'w') as f:
             json.dump(data, f, ensure_ascii=False, sort_keys=True, indent=2)
+    logger.info(f'Saving {key}')
